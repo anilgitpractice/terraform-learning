439289b646afa4fd6126d9f788de4573e70b58ee
anilgitpractice <itsmeanilpagadala@gmail.com>
2022-12-10 13:40:51 +0000

    commit on the removing  vpc public route table

--- main.tf
+++ main.tf
@@ -61,28 +61,6 @@ resource "aws_route_table_association" "public_1_rt_a" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
-resource "aws_route_table" "private_rt" {
-  vpc_id = aws_vpc.my_custom_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.some_ig.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.some_ig.id
-  }
-
-  tags = {
-    Name = "Private Route Table"
-  }
-}
-
-resource "aws_route_table_association" "private_1_rt_a" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
 
 resource "aws_security_group" "web_sg" {
   name   = "HTTP and SSH"
